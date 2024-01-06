90e7b1530f0a5c059167a8eede1e73eddf33fd4e
Anthony Guamaninga <anthonysub300@gmail.com>
2024-01-06 11:00:30 -0500

    taller20

---
+++ src/components/Pregunta.vue
@@ -0,0 +1,33 @@
+<template>
+ <img src='https://yesno.wtf/assets/yes/8-2f93962e2ab24427df8589131da01a4d.gif' 
+ alt="No se puede visualizar"
+ />
+
+<div class="dark">
+
+</div>
+<div class="container">
+
+<div class="container">
+<input type="text">
+<p> Recuerda que debes ...</p>
+    <h2>voy a pasar el semestre</h1>
+    <h1>SI, NO........ </h1>
+    </div>
+</template>
+<script>
+
+</script>
+export default{};
+<style scoped>
+.dark{
+height:100vh;
+with: 100vw;
+max-height:100%;
+max-width:100%;
+position: fixed;
+top: 0px;
+left:0px;
+background-color: rgba(0,0,0,5)
+}
+</style>
